4caa5a323426a9967f7399fca7bbbf1c8c4d3ef5
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 19:44:42 +0530

    Make a Api for message in index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 require('./config/connection');
 
 const AuthRoutes = require('./routes/authRouter');
+const MessageRoutes = require('./routes/messageRouter');
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
@@ -17,6 +18,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", AuthRoutes);
+app.use("/api/message", MessageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
