df61773b7dfb8a575d54410d0c62fa9ba5781a80
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 19:45:12 +0530

    Create a message model

---
+++ models/messageModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    senderId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    receiverId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    text: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+}, { timestamps: true });
+
+const Message = mongoose.model('Message', messageSchema);
+
+module.exports = Message;
