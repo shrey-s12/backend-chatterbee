ad6c3a86de01dc17b6a2bd42dc80ca96c7bdd7bd
Shrey Singhal <shreynbd12@gmail.com>
2025-02-19 19:46:01 +0530

    Create a message controllers

---
+++ controllers/messageControllers.js
@@ -0,0 +1,66 @@
+const USER = require("../models/userModel");
+const MESSAGE = require("../models/messageModel");
+
+const getUsersForSidebar = async (req, res) => {
+    try {
+        const loggedInUserId = req.user._id;
+        const filteredUsers = await USER.find({ _id: { $ne: loggedInUserId } }).select("-password");
+
+        res.status(200).json(filteredUsers);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await MESSAGE.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, receiverId: myId }
+            ]
+        });
+
+        res.status(200).json(messages);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if (image) {
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new MESSAGE({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        });
+
+        await newMessage.save();
+
+        // todo: realTime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+}
+
+module.exports = {
+    getUsersForSidebar,
+    getMessages,
+    sendMessage
+};
