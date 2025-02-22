28c28ab830980f1d1f304b4854b4f4fc54a29ef7
Shrey Singhal <shreynbd12@gmail.com>
2025-02-22 15:45:30 +0530

    set the image size limit

--- index.js
+++ index.js
@@ -11,12 +11,13 @@ const MessageRoutes = require('./routes/messageRouter');
 const PORT = process.env.PORT;
 const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
     origin: CLIENT_URL,
     credentials: true
 }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", AuthRoutes);
 app.use("/api/messages", MessageRoutes);
